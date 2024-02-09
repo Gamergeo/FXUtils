dedcd760e29a196598c0e629a0ba6977cb68ac3a
gleva <gleva@Rico>
2024-02-09 19:22:29 +0100

    Scope redefinition

--- src/main/java/com/gamergeo/lib/viewmodelfx/parameter/ApplicationParameters.java
+++
@@ -1,9 +0,0 @@
-package com.gamergeo.lib.viewmodelfx.parameter;
-
-public interface ApplicationParameters {
-	
-    <T> void addApplicationParameter(String name, Class<T> parameterType);
-
-	Parameter<?> getApplicationParameter(String name);
-
-}
--- src/main/java/com/gamergeo/lib/viewmodelfx/view/FXUtils.java
+++ src/main/java/com/gamergeo/lib/viewmodelfx/view/FXUtils.java
@@ -4,11 +4,15 @@ import com.gamergeo.lib.viewmodelfx.listener.EmptyChangeListener;
 import com.gamergeo.lib.viewmodelfx.listener.SimpleChangeListener;
 import com.gamergeo.lib.viewmodelfx.listener.SimpleListChangeListener;
 
+import de.saxsys.mvvmfx.FluentViewLoader;
+import de.saxsys.mvvmfx.FxmlView;
+import de.saxsys.mvvmfx.ViewModel;
 import javafx.beans.property.Property;
 import javafx.beans.property.ReadOnlyProperty;
 import javafx.collections.ObservableList;
+import javafx.scene.Parent;
 
-public class ViewUtils {
+public class FXUtils {
     public static <T> void bindDirectional(Property<T> property1, Property<T> property2) {
         property2.setValue(property1.getValue());
         property1.addListener((observable, oldValue, newValue) -> {
@@ -29,5 +33,22 @@ public class ViewUtils {
 
     public static <T> void addSimpleListChangeListener(ObservableList<T> list, SimpleListChangeListener<T> listener) {
         list.addListener(listener);
+    }
+    
+    /**
+     * Extends fxml view to add a specific css.
+     * Use this method to get parent
+     */
+    public static <ViewType extends FxmlView<? extends ViewModelType>, ViewModelType extends ViewModel> Parent getRoot(Class<ViewType> viewClass) {
+    	Parent parent = FluentViewLoader.fxmlView(viewClass).load().getView();
+        String resourcePath = classPackageToResourcePath(viewClass);
+        parent.getStylesheets().add(resourcePath);
+        return parent;
+    }
+    
+    public static String classPackageToResourcePath(Class<?> clazz) {
+        String packageName = clazz.getPackage().getName();
+        String resourcePath = packageName.replace('.', '/');
+        return resourcePath + "/" + clazz.getSimpleName() + ".css";
     }
 }
